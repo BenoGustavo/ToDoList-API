6a490f29831c5c5a5c986a8bc2adfe361a47b1b7
Gustavo <gustavoleandrogorges@gmail.com>
2024-07-31 23:50:51 -0300

    refactor: CORS + Auth settings

--- src/main/java/dev/gustavo/ToDoListAPI/config/SecurityConfig.java
+++ src/main/java/dev/gustavo/ToDoListAPI/config/SecurityConfig.java
@@ -1,21 +1,80 @@
 package dev.gustavo.ToDoListAPI.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import dev.gustavo.ToDoListAPI.filter.JwtRequestFilter;
+import dev.gustavo.ToDoListAPI.user.service.CustomUserDetailsService;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(authorizeRequests -> authorizeRequests
-                        .anyRequest().permitAll())
-                .csrf().disable();
-        return http.build();
-    }
+        final static String[] ALLOWED_ROUTES_WITHOUT_AUTH = {
+                        "/auth/**", "/users/"
+        };
+
+        @Autowired
+        private CustomUserDetailsService customUserDetailsService;
+
+        @Autowired
+        private JwtRequestFilter jwtRequestFilter;
+
+        @Bean
+        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+                http
+                                .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                                                .requestMatchers(ALLOWED_ROUTES_WITHOUT_AUTH).permitAll()
+                                                .anyRequest().authenticated())
+                                .csrf(csrf -> csrf.disable())
+                                .sessionManagement(sessionManagement -> sessionManagement
+                                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+                http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+
+                return http.build();
+        }
+
+        @Bean
+        public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+                AuthenticationManagerBuilder authenticationManagerBuilder = http
+                                .getSharedObject(AuthenticationManagerBuilder.class);
+
+                authenticationManagerBuilder
+                                .userDetailsService(customUserDetailsService)
+                                .passwordEncoder(passwordEncoder());
+
+                return authenticationManagerBuilder.build();
+        }
+
+        @Bean
+        public PasswordEncoder passwordEncoder() {
+                return new BCryptPasswordEncoder();
+        }
+
+        @Bean
+        public WebMvcConfigurer corsConfigurer() {
+                return new WebMvcConfigurer() {
+                        @Override
+                        public void addCorsMappings(@NonNull CorsRegistry registry) {
+                                registry.addMapping("/**")
+                                                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                                                .allowedOrigins("*")
+                                                .allowedHeaders("*");
+                        }
+                };
+        }
 }
