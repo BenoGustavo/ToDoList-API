62068c2c2653d552d178aa832b6268f69ecb8910
Gustavo <gustavoleandrogorges@gmail.com>
2024-07-31 23:47:16 -0300

    feat: utils jwt methods

---
+++ src/main/java/dev/gustavo/ToDoListAPI/utils/JwtUtil.java
@@ -0,0 +1,76 @@
+package dev.gustavo.ToDoListAPI.utils;
+
+import java.util.Arrays;
+import java.util.Base64;
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@Component
+public class JwtUtil {
+
+    private final byte[] secretKey;
+
+    @Autowired
+    public JwtUtil(Environment env) {
+        String secretKeyString = env.getProperty("JWT_SECRET_KEY");
+
+        if (secretKeyString == null) {
+            throw new RuntimeException("JWT_SECRET_KEY is not set");
+        }
+
+        this.secretKey = Base64.getDecoder().decode(secretKeyString);
+        System.out.println("Decoded JWT_SECRET_KEY (byte array): " + Arrays.toString(secretKey));
+    }
+
+    public String generateToken(Authentication authentication) {
+        String username = authentication.getName();
+
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60)) // 1 hour
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+    }
+
+    public String generateToken(UserDetails userDetails) {
+        String username = userDetails.getUsername();
+
+        System.out.println("Generating token for user: " + username);
+
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60)) // 1 hour
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return extractClaims(token).getSubject();
+    }
+
+    public Claims extractClaims(String token) {
+        return Jwts.parser()
+                .setSigningKey(secretKey)
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public boolean isTokenExpired(String token) {
+        return extractClaims(token).getExpiration().before(new Date());
+    }
+
+    public boolean validateToken(String token, String username) {
+        return (username.equals(extractUsername(token)) && !isTokenExpired(token));
+    }
+}
