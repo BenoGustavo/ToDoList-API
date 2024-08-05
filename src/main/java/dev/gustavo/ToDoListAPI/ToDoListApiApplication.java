def43f35610ac64d6957a76cbff31e8c3d5240ca
Gustavo <gustavoleandrogorges@gmail.com>
2024-08-04 22:43:38 -0300

    refactor: Enable JPA repositories for ToDoListApiApplication

--- src/main/java/dev/gustavo/ToDoListAPI/ToDoListApiApplication.java
+++ src/main/java/dev/gustavo/ToDoListAPI/ToDoListApiApplication.java
@@ -2,8 +2,10 @@ package dev.gustavo.ToDoListAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "dev.gustavo.ToDoListAPI.repositories.interfaces")
 public class ToDoListApiApplication {
 
 	public static void main(String[] args) {
