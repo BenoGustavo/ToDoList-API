4a7a30ad0b60ddca189e9e7a2ca25f4edb9b6e32
Gustavo <gustavoleandrogorges@gmail.com>
2024-08-09 18:13:15 -0300

    feat: Update IUserService to handle profile picture updates with MultipartFile

--- src/main/java/dev/gustavo/ToDoListAPI/service/UserService.java
+++ src/main/java/dev/gustavo/ToDoListAPI/service/UserService.java
@@ -1,13 +1,20 @@
 package dev.gustavo.ToDoListAPI.service;
 
-import java.sql.Blob;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.sql.SQLException;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
+import javax.imageio.ImageIO;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import dev.gustavo.ToDoListAPI.models.UserModel;
 import dev.gustavo.ToDoListAPI.repositories.interfaces.IUserRepository;
@@ -71,13 +78,20 @@ public class UserService implements IUserService {
     }
 
     @Override
-    public UserDTO create(UserModel user) {
+    public UserDTO create(UserModel user) throws IOException {
         // Validate user model
         isUserModelValid(user);
+
+        if (!EmailValidator.isValid(user.getEmail())) {
+            throw new BadRequest400Exception("Invalid email");
+        }
 
         // Hash password
         String hashedPassword = BCrypt.hashpw(user.getHashedPassword(), BCrypt.gensalt());
         user.setHashedPassword(hashedPassword);
+
+        // Set default picture
+        user.setProfilePicture(getDefaultPicture());
 
         // Save on database
         userRepository.save(user);
@@ -111,12 +125,20 @@ public class UserService implements IUserService {
     }
 
     @Override
-    public UserDTO update(UUID id, UserDTO user) throws NotFound404Exception, BadRequest400Exception {
-        // isUserAuthorized(request);
-        isUserModelValid(user);
+    public UserDTO update(UUID id, UserDTO user, HttpServletRequest request)
+            throws NotFound404Exception, BadRequest400Exception {
+        isUserAuthorized(request);
 
-        if (!EmailValidator.isValid(user.getEmail())) {
-            throw new BadRequest400Exception("Invalid email");
+        String authHeader = request.getHeader("Authorization");
+        String token = authHeader.substring(7);
+        String userEmail = jwtUtil.extractEmail(token);
+        UserModel userThatRequested = userRepository.findByEmail(userEmail);
+
+        System.out.println(userThatRequested.getId() + " " + id + " " + userThatRequested.getId().equals(id) + " "
+                + userEmail);
+
+        if (!userThatRequested.getId().equals(id)) {
+            throw new Unauthorized401Exception("Don't have permission to access this user");
         }
 
         // Get the user from database or throw
@@ -135,9 +157,22 @@ public class UserService implements IUserService {
     }
 
     @Override
-    public UserDTO updateProfilePicture(UUID id, Blob newPicture) throws NotFound404Exception, BadRequest400Exception {
+    public UserDTO updateProfilePicture(UUID id, MultipartFile newPicture, HttpServletRequest request)
+            throws NotFound404Exception, BadRequest400Exception, SQLException, IOException {
 
-        // isUserAuthorized(request);
+        String authHeader = request.getHeader("Authorization");
+        String token = authHeader != null ? authHeader.substring(7) : null;
+
+        if (token == null || token.isEmpty()) {
+            throw new Unauthorized401Exception("Authorization token is missing");
+        }
+
+        String userEmail = jwtUtil.extractEmail(token);
+        UserModel userThatRequested = userRepository.findByEmail(userEmail);
+
+        if (!userThatRequested.getId().equals(id)) {
+            throw new Unauthorized401Exception("Don't have permission to access this user");
+        }
 
         if (newPicture == null) {
             throw new BadRequest400Exception("Invalid picture");
@@ -147,7 +182,9 @@ public class UserService implements IUserService {
         UserModel userModel = userRepository.findById(id)
                 .orElseThrow(() -> new NotFound404Exception("User not found"));
 
-        userModel.setProfilePicture(newPicture);
+        byte[] picture = newPicture.getBytes();
+
+        userModel.setProfilePicture(picture);
         userModel.setUpdatedAt(LocalDateTime.now());
 
         // Save changes on database
@@ -194,36 +231,6 @@ public class UserService implements IUserService {
 
     private boolean isPasswordValid(String newPassword) {
         return (newPassword != null) && (!newPassword.isEmpty()) && (newPassword.length() >= 5);
-    }
-
-    // This method wiil check if the user model is in pair with the business rules
-    private boolean isUserModelValid(UserDTO user) throws BadRequest400Exception {
-
-        if (!EmailValidator.isValid(user.getEmail())) {
-            throw new BadRequest400Exception("Invalid email");
-        }
-
-        if (user.getFirstName().length() < 3 || user.getFirstName().length() > 50) {
-            throw new BadRequest400Exception("Invalid first name length");
-        }
-
-        if (user.getLastName().length() < 3 || user.getLastName().length() > 50) {
-            throw new BadRequest400Exception("Invalid last name length");
-        }
-
-        if (user.getNickname().length() < 3 || user.getNickname().length() > 50) {
-            throw new BadRequest400Exception("Invalid nickname length");
-        }
-
-        if (user.getEmail().length() < 3 || user.getEmail().length() > 50) {
-            throw new BadRequest400Exception("Invalid email length");
-        }
-
-        if (user.getAge() < 10 || user.getAge() > 120) {
-            throw new BadRequest400Exception("Invalid age");
-        }
-
-        return true;
     }
 
     // This method wiil check if the user model is in pair with the business rules
@@ -281,12 +288,7 @@ public class UserService implements IUserService {
             isUpdated = true;
         }
 
-        if (isFieldUpdated(userDTO.getEmail(), userModel.getEmail())) {
-            userModel.setEmail(userDTO.getEmail());
-            isUpdated = true;
-        }
-
-        if (userDTO.getAge() != userModel.getAge()) {
+        if (userDTO.getAge() != userModel.getAge() && userDTO.getAge() > 9 && userDTO.getAge() < 121) {
             userModel.setAge(userDTO.getAge());
             isUpdated = true;
         }
@@ -309,6 +311,16 @@ public class UserService implements IUserService {
         if (token == null || token.isEmpty()) {
             throw new Unauthorized401Exception("Authorization token is missing");
         }
+    }
+
+    public byte[] getDefaultPicture() throws IOException {
+        File imageFile = new File("src/main/resources/static/icon/image.png");
+        // Read the image
+        BufferedImage image = ImageIO.read(imageFile);
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ImageIO.write(image, "png", baos);
+        return baos.toByteArray();
     }
 
 }
--- src/main/java/dev/gustavo/ToDoListAPI/service/interfaces/IUserService.java
+++ src/main/java/dev/gustavo/ToDoListAPI/service/interfaces/IUserService.java
@@ -1,8 +1,11 @@
 package dev.gustavo.ToDoListAPI.service.interfaces;
 
-import java.sql.Blob;
+import java.io.IOException;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.UUID;
+
+import org.springframework.web.multipart.MultipartFile;
 
 import dev.gustavo.ToDoListAPI.models.UserModel;
 import dev.gustavo.ToDoListAPI.utils.error.custom.BadRequest400Exception;
@@ -26,70 +29,71 @@ import jakarta.servlet.http.HttpServletRequest;
  */
 public interface IUserService {
 
-    /**
-     * Retrieves a user by their unique identifier.
-     *
-     * @param id the unique identifier of the user
-     * @return the user data transfer object
-     */
-    UserDTO getUserById(UUID id, HttpServletRequest request);
+        /**
+         * Retrieves a user by their unique identifier.
+         *
+         * @param id the unique identifier of the user
+         * @return the user data transfer object
+         */
+        UserDTO getUserById(UUID id, HttpServletRequest request);
 
-    /**
-     * Retrieves all users.
-     *
-     * @return a list of user data transfer objects
-     */
-    List<UserDTO> getAllUsers();
+        /**
+         * Retrieves all users.
+         *
+         * @return a list of user data transfer objects
+         */
+        List<UserDTO> getAllUsers();
 
-    /**
-     * Retrieves a user by their email.
-     *
-     * @param email the email of the user
-     * @return the user data transfer object
-     */
-    UserDTO getUserByEmail(String email, HttpServletRequest request);
+        /**
+         * Retrieves a user by their email.
+         *
+         * @param email the email of the user
+         * @return the user data transfer object
+         */
+        UserDTO getUserByEmail(String email, HttpServletRequest request);
 
-    /**
-     * Creates a new user.
-     *
-     * @param user the user data transfer object (DTO)
-     * 
-     * @return the created user data transfer object
-     */
-    UserDTO create(UserModel user);
+        /**
+         * Creates a new user.
+         *
+         * @param user the user data transfer object (DTO)
+         * 
+         * @return the created user data transfer object
+         */
+        UserDTO create(UserModel user) throws IOException;
 
-    /**
-     * Updates an existing user.
-     *
-     * @param user the user data transfer object
-     * @return the updated user data transfer object
-     */
-    UserDTO update(UUID id, UserDTO user);
+        /**
+         * Updates an existing user.
+         *
+         * @param user the user data transfer object
+         * @return the updated user data transfer object
+         */
+        UserDTO update(UUID id, UserDTO user, HttpServletRequest request);
 
-    /**
-     * Deletes a user by their unique identifier.
-     *
-     * @param id the unique identifier of the user
-     * @return the deleted user data transfer object
-     */
-    UserDTO delete(UUID id, HttpServletRequest request);
+        /**
+         * Deletes a user by their unique identifier.
+         *
+         * @param id the unique identifier of the user
+         * @return the deleted user data transfer object
+         */
+        UserDTO delete(UUID id, HttpServletRequest request);
 
-    /**
-     * Updates the user profile picture.
-     *
-     * @param newPicture the picture in a blob format
-     * @param id         the unique identifier of the user
-     * @return the updated user data transfer object
-     */
-    UserDTO updateProfilePicture(UUID id, Blob newPicture) throws NotFound404Exception, BadRequest400Exception;
+        /**
+         * Updates the user profile picture.
+         *
+         * @param newPicture the picture in a blob format
+         * @param id         the unique identifier of the user
+         * @return the updated user data transfer object
+         */
+        public UserDTO updateProfilePicture(UUID id, MultipartFile newPicture, HttpServletRequest request)
+                        throws NotFound404Exception, BadRequest400Exception, SQLException, IOException;
 
-    /**
-     * Updates the user profile picture.
-     *
-     * @param newPassword The new password
-     * @param id          the unique identifier of the user
-     * @return the updated user data transfer object
-     */
-    UserDTO updatePassword(String email, String newPassword, String oldPassword, HttpServletRequest request)
-            throws NotFound404Exception;
+        /**
+         * Updates the user profile picture.
+         *
+         * @param newPassword The new password
+         * @param id          the unique identifier of the user
+         * @return the updated user data transfer object
+         */
+        UserDTO updatePassword(String email, String newPassword, String oldPassword, HttpServletRequest request)
+                        throws NotFound404Exception;
 }
