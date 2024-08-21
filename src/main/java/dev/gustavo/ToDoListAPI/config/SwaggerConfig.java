24900a269b35496f0e1f78d25ceb0dac3b341676
Gustavo <gustavoleandrogorges@gmail.com>
2024-08-21 17:03:26 -0300

    refactor: Update SwaggerConfig to use consistent formatting and improve code structure

--- src/main/java/dev/gustavo/ToDoListAPI/config/SwaggerConfig.java
+++ src/main/java/dev/gustavo/ToDoListAPI/config/SwaggerConfig.java
@@ -13,28 +13,29 @@ import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
 public class SwaggerConfig {
-    private static final String[] END_POINTS = {
-            "/auth/**", "/users/**", "/tasks/**", "/taskbundles/**"
-    };
+        private static final String[] END_POINTS = {
+                        "/auth/**", "/users/**", "/tasks/**", "/taskbundles/**"
+        };
 
-    @Bean
-    public GroupedOpenApi publicApi() {
-        GroupedOpenApi.Builder groupedOpenApi = GroupedOpenApi.builder();
-        return groupedOpenApi.pathsToMatch(END_POINTS).group("public-api").build();
-    }
+        @Bean
+        public GroupedOpenApi publicApi() {
+                GroupedOpenApi.Builder groupedOpenApi = GroupedOpenApi.builder();
+                return groupedOpenApi.pathsToMatch(END_POINTS).group("public-api").build();
+        }
 
-    @Bean
-    OpenAPI customOpenAPI() {
-        return new OpenAPI()
-                .info(new Info()
-                        .title("ToDo List API")
-                        .version("1.0")
-                        .description("A simple ToDo List API")
-                        .termsOfService("http://swagger.io/terms/")
-                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
-                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
-                .components(new Components()
-                        .addSecuritySchemes("bearerAuth", new SecurityScheme().type(SecurityScheme.Type.HTTP)
-                                .scheme("bearer").bearerFormat("JWT")));
-    }
+        @Bean
+        public OpenAPI customOpenAPI() {
+                return new OpenAPI()
+                                .info(new Info()
+                                                .title("ToDo List API")
+                                                .version("1.0")
+                                                .description("A simple ToDo List API")
+                                                .termsOfService("http://swagger.io/terms/")
+                                                .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+                                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                                .components(new Components()
+                                                .addSecuritySchemes("bearerAuth",
+                                                                new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                                                                                .scheme("bearer").bearerFormat("JWT")));
+        }
 }
